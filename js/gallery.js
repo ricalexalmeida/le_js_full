48d0e9244ada319926f1b65ea38a59c169cd1bfc
Ricardo Almeida <ricardo94almeida@gmail.com>
2018-11-03 18:53:03 +0000

    Change main image to match current thumbnail

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,13 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#galley-photo > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+      mainImage.setAttribute("alt", thumbnail.alt);
+    })
+  })
 }
