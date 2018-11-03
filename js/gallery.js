aaa03c31278487891b9d4c07d1416d42a2fc9a8a
Ricardo Almeida <ricardo94almeida@gmail.com>
2018-11-03 18:56:14 +0000

    Fix typo in gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,6 @@
 function activateGallery() {
-  let thumbnails = document.querySelector("#gallery-thumbs").
-                            querySelectorAll("img");
-  let mainImage = document.querySelector("#galley-photo > img");
+  let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
